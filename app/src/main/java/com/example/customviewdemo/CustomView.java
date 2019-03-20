3cfa7ac5aa2a1472ecd87a101bbedfeb353245ba
komal-nistane <komal.nistane@globallogic.com>
2019-03-20 16:14:03 +0530

    Unused code removed

--- app/src/main/java/com/example/customviewdemo/CustomView.java
+++ app/src/main/java/com/example/customviewdemo/CustomView.java
@@ -1,42 +1,34 @@
 package com.example.customviewdemo;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 
 public class CustomView extends AppCompatButton {
 
-    Paint mPaint;
-    Rect mRect;
-    int mSquareColor;
     com.example.customviewdemo.CustomView mSubmit;
 
 
     public CustomView(Context context) {
         super(context);
-        init(null);
+        init();
     }
 
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs);
+        init();
     }
 
     public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(attrs);
+        init();
     }
 
 
-    private void init(@Nullable AttributeSet set){
-        mSubmit =(com.example.customviewdemo.CustomView) findViewById(R.id.custom_button_view);
+    private void init(){
+        mSubmit =(CustomView) findViewById(R.id.custom_button_view);
         StringBuilder buttonText = new StringBuilder("Submit");
         StringBuilder reverseName = buttonText.reverse();
         this.setText(reverseName);
